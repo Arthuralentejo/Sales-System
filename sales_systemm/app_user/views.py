47378c1b9ce785d7a0d019da549062b77d76ccb8
Plataforma Software <plataforma.software@capstone.local>
2024-11-25 10:02:08 -0300

    criando Django

--- sales_systemm/app_user/views.py
+++ sales_systemm/app_user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import User
 
 def home(request):
@@ -6,12 +7,18 @@ def home(request):
 
 
 def user_list(request):
-
-    new_user = User()
-    new_user.first_name = request.POST.get('first_name')
-    new_user.last_name = request.POST.get('last_name')
-    new_user.document_id = request.POST.get('document_id')
-    new_user.save()
+    if request.method == 'POST':
+        document_id = request.POST.get('document_id')
+        if User.objects.filter(document_id=document_id).exists():
+            messages.error(request, 'Documento já cadastrado')
+            return render(request, 'home.html')
+        else:
+            new_user = User()
+            new_user.first_name = request.POST.get('first_name')
+            new_user.last_name = request.POST.get('last_name')
+            new_user.document_id = document_id
+            new_user.save()
+            messages.success(request, 'User registered successfully.')
 
     users = {
         'users': User.objects.all()
