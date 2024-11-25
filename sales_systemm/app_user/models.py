79c3cf756a9b0a25e4398bae710b9e6109c915b8
Plataforma Software <plataforma.software@capstone.local>
2024-11-25 10:29:42 -0300

    Adicionando Templates e Model

--- sales_systemm/app_user/models.py
+++ sales_systemm/app_user/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class User (models.Model):
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     document_id = models.CharField(max_length=100, default='default_value')
-    # document_id = models.CharField(max_length=100, null=True)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
